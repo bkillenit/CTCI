2365d55ba8e73b1d5dff25209331287f0e893415
bkillenit <blaine.j.killen@gmail.com>
2015-08-02 18:46:43 -0500

    completed checkPermutation algo

--- 1ch/InterviewQuestions.js
+++ 1ch/InterviewQuestions.js
@@ -12,3 +12,34 @@ var isUnique = function( str ) {
 
 	return true;
 };
+
+// given two strings, write a method to decide if one is a permutation of the other
+var checkPermutation = function( str1, str2 ) {
+	function hashCharCountForString( str ) {
+		var charHash = {};
+		var strArray = str.split('');
+
+		strArray.forEach(function( char ) {
+			if( charHash[char] === undefined ) charHash[char] = 1;
+			else charHash[char] += 1;
+		});
+
+		return charHash;
+	}
+
+	function checkCharHash( charHash1, charHash2, keysChecked = [] ) {
+		Object.keys(charHash).forEach(function( key ) {
+			if( !(key in keysChecked) )
+				if( charHash1[key] != charHash2[key] ) return false;
+				else keysChecked.push(key);
+		});
+
+		return true;
+	}
+
+	var charHash1 = hashCharCountForString(str1);
+	var charHash2 = hashCharCountForString(str2);
+	var keysChecked = [];
+
+	return ( checkCharHash(charHash1 , charHash2, keysChecked) && checkCharHash(charHash2, charHash1, keysChecked) );
+};
