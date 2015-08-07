2bf5500ccd47065929170749e83f8d1b0ab0a21d
bkillenit <blaine.j.killen@gmail.com>
2015-08-07 17:34:38 -0500

    implemented remove dups challenge

--- 2ch/InterviewQuestions.js
+++ 2ch/InterviewQuestions.js
@@ -1,5 +1,7 @@
 // write code to remove duplicates from an unsorted linked list. How would you solve this problem if a temporary buffer is
 // not allowed?
+// Worse case: O( (n - 1 ) + ( n - 2 ) ... 2 + 1 ) reduces to O( (n*(n+1))/2 ) which finally reduces to O(n^2) iterations
+// Best case: O(n - 1) or simply O(n)
 var removeDups = function( linkedList ) {
 	var pointer = linkedList.head;
 	var dupPointer = pointer.next;
